4b7967049b84f838a2f082c58d8c3df3af5722fa
JB AUBREE <jbaubree@MacBook-Pro-de-JB.local>
2022-08-18 14:53:25 +0200

    feat: export resolveOptions

--- src/index.ts
+++ src/index.ts
@@ -32,5 +32,6 @@ export function generateSitemap(options: UserOptions = {}) {
 }
 
 export { getRoutes, getFormattedSitemap } from './sitemap'
+export { resolveOptions } from './options'
 export * from './types'
 export default generateSitemap
