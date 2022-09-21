5eee758d86f005536911a77b81c0cc8231c42973
JB <jb.aubree@protonmail.com>
2022-09-21 20:29:44 +0200

    fix(opt): accept absolute path for outdir

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,10 @@
-import { resolve } from 'path'
+import { isAbsolute, resolve } from 'path'
 import { ensurePrefix } from '@antfu/utils'
 
 import type { ResolvedOptions } from './types'
 
 export function getResolvedPath(file: string, resolvedOptions: ResolvedOptions) {
+  if (isAbsolute(resolvedOptions.outDir))
+    return resolve(`${resolvedOptions.outDir}/${file}`)
   return resolve(`${ensurePrefix('./', resolvedOptions.outDir)}/${file}`)
 }
---
+++ test/utils.test.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { describe, expect, test } from 'vitest'
+import { ensurePrefix } from '@antfu/utils'
+
+import { getResolvedPath } from '../src/utils'
+import { resolveOptions } from '../src/options'
+
+describe('Utils', () => {
+  test('Get resolved path', async () => {
+    const opt = resolveOptions({})
+    const optAbsolute = resolveOptions({ outDir: resolve('.') })
+    const file = 'test.txt'
+
+    expect(getResolvedPath(file, opt))
+      .toEqual(resolve(`${ensurePrefix('./', opt.outDir)}/${file}`))
+
+    expect(getResolvedPath(file, optAbsolute))
+      .toEqual(resolve(`${optAbsolute.outDir}/${file}`))
+  })
+})
