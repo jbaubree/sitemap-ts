92d56fb71858d25842c6ee15045f7c5eb9d9e90f
userquin <userquin@gmail.com>
2025-07-18 21:36:45 +0200

    feat(i18n): add `prefix_except_default` strategy

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -1,5 +1,5 @@
 import { join, parse } from 'node:path'
-import { ensurePrefix, slash } from '@antfu/utils'
+import { ensurePrefix, ensureSuffix, slash } from '@antfu/utils'
 import fg from 'fast-glob'
 
 import type { ResolvedOptions, RoutesOptionMap } from './types'
@@ -36,6 +36,15 @@ function getOptionByRoute<T extends Date | string | number>(options: T | RoutesO
 }
 
 export function getFormattedSitemap(options: ResolvedOptions, routes: string[]) {
+  if (options.i18n?.strategy === 'prefix_except_default') {
+    return routes.map(prefixExceptDefaultLanguageFactory(
+      options,
+      {
+        defaultLanguage: options.i18n.defaultLanguage ?? 'en',
+        languages: options.i18n.languages,
+      },
+    ))
+  }
   return routes.map((route) => {
     const hostNamePath = removeMaybeSuffix('/', options.hostname)
     const routePath = options.basePath ? ensurePrefix('/', options.basePath) + ensurePrefix('/', route) : ensurePrefix('/', route)
@@ -57,4 +66,103 @@ export function getFormattedSitemap(options: ResolvedOptions, routes: string[]) 
 
     return formattedSitemap
   })
+}
+
+interface PrefixExceptDefaultStrategy {
+  defaultLanguage: string
+  languages: string[]
+}
+
+interface XHTMLanguageLink {
+  hreflang: string
+  rel: string
+  href: string
+}
+
+interface SiteMapRouteEntry {
+  url: string
+  changefreq?: string
+  priority?: number
+  lastmod?: Date | string | number
+  links: XHTMLanguageLink[]
+}
+
+function prefixExceptDefaultLanguageFactory(
+  options: ResolvedOptions,
+  i18n: PrefixExceptDefaultStrategy,
+) {
+  // we need N links, where N is the number of languages, the x-default should always use the default language link for the target route
+  // we also need to handle special / cases like /en, /fr, etc. We need to ensure that the loc and xhtml:link always with the trailing /
+  const hostNamePath = removeMaybeSuffix('/', options.hostname)
+  const useBasePath = ensurePrefix('/', ensureSuffix('/', options.basePath))
+  const contextPath = removeMaybeSuffix('/', useBasePath)
+  const defaultLanguage = i18n.defaultLanguage ?? 'en'
+  const locales = i18n.languages.filter(l => l !== defaultLanguage)
+  const localePrefixes = locales.map((lang) => {
+    return [`${contextPath}/${lang}/`, `${contextPath}/${lang}`, lang] as const
+  })
+
+  return (route: string) => {
+    let url: string
+    let pathWithoutLang: string
+    if (route === useBasePath) {
+      url = ensureSuffix('/', new URL(useBasePath, hostNamePath).href)
+      pathWithoutLang = useBasePath[0] === '/' ? useBasePath.slice(1) : useBasePath
+    }
+    else {
+      const locale = localePrefixes.find(([
+        prefix1,
+        prefix2,
+      ]) => route.startsWith(prefix1) || route.startsWith(prefix2),
+      )
+      if (locale) {
+        url = route.startsWith(locale[0])
+          ? route.replace(locale[0], '')
+          : route.replace(locale[1], '')
+        if (url === '') {
+          pathWithoutLang = useBasePath[0] === '/' ? useBasePath.slice(1) : useBasePath
+          url = ensureSuffix('/', new URL(`${useBasePath}${locale[2]}`, hostNamePath).href)
+        }
+        else {
+          pathWithoutLang = url.replace(useBasePath, '')
+          url = new URL(route, hostNamePath).href
+        }
+      }
+      else {
+        pathWithoutLang = route.replace(useBasePath, '')
+        url = new URL(route, hostNamePath).href
+      }
+    }
+    const trailingSlash = url.at(-1) === '/'
+    let xDefaultHref = new URL(`${useBasePath}${pathWithoutLang}`, hostNamePath).href
+    if (trailingSlash)
+      xDefaultHref = ensureSuffix('/', xDefaultHref)
+
+    const links: XHTMLanguageLink[] = locales.map((l) => {
+      const href = new URL(`${useBasePath}${l}/${pathWithoutLang}`, hostNamePath).href
+      return {
+        hreflang: l,
+        rel: 'alternate',
+        href: trailingSlash ? ensureSuffix('/', href) : href,
+      }
+    }).concat({
+      hreflang: 'x-default',
+      rel: 'alternate',
+      href: xDefaultHref,
+    })
+
+    links.unshift({
+      hreflang: defaultLanguage,
+      rel: 'alternate',
+      href: xDefaultHref,
+    })
+
+    return {
+      url,
+      changefreq: getOptionByRoute(options.changefreq, route) ?? defaultOptions.changefreq,
+      priority: getOptionByRoute(options.priority, route) ?? defaultOptions.priority,
+      lastmod: getOptionByRoute(options.lastmod, route) ?? defaultOptions.lastmod,
+      links,
+    } satisfies SiteMapRouteEntry
+  }
 }
--- test/sitemap.test.ts
+++ test/sitemap.test.ts
@@ -258,5 +258,108 @@ describe('sitemap', () => {
         },
       ]
     `)
+    expect(getFormattedSitemap(resolveOptions({
+      i18n: {
+        languages: ['fr', 'en'],
+        defaultLanguage: 'fr',
+        strategy: 'prefix_except_default',
+      },
+      lastmod: new Date('2025-07-18T12:00:00.00Z'),
+    }), ['/', '/route-a', '/en/', '/en/route-a'])).toMatchInlineSnapshot(`
+      [
+        {
+          "changefreq": "daily",
+          "lastmod": 2025-07-18T12:00:00.000Z,
+          "links": [
+            {
+              "href": "http://localhost/",
+              "hreflang": "fr",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/en/",
+              "hreflang": "en",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/",
+              "hreflang": "x-default",
+              "rel": "alternate",
+            },
+          ],
+          "priority": 1,
+          "url": "http://localhost/",
+        },
+        {
+          "changefreq": "daily",
+          "lastmod": 2025-07-18T12:00:00.000Z,
+          "links": [
+            {
+              "href": "http://localhost/route-a",
+              "hreflang": "fr",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/en/route-a",
+              "hreflang": "en",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/route-a",
+              "hreflang": "x-default",
+              "rel": "alternate",
+            },
+          ],
+          "priority": 1,
+          "url": "http://localhost/route-a",
+        },
+        {
+          "changefreq": "daily",
+          "lastmod": 2025-07-18T12:00:00.000Z,
+          "links": [
+            {
+              "href": "http://localhost/",
+              "hreflang": "fr",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/en/",
+              "hreflang": "en",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/",
+              "hreflang": "x-default",
+              "rel": "alternate",
+            },
+          ],
+          "priority": 1,
+          "url": "http://localhost/en/",
+        },
+        {
+          "changefreq": "daily",
+          "lastmod": 2025-07-18T12:00:00.000Z,
+          "links": [
+            {
+              "href": "http://localhost/route-a",
+              "hreflang": "fr",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/en/route-a",
+              "hreflang": "en",
+              "rel": "alternate",
+            },
+            {
+              "href": "http://localhost/route-a",
+              "hreflang": "x-default",
+              "rel": "alternate",
+            },
+          ],
+          "priority": 1,
+          "url": "http://localhost/en/route-a",
+        },
+      ]
+    `)
   })
 })
